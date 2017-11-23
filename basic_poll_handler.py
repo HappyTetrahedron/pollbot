dc947b40a18aedf78e3be08acd44441f960cee33
Aline Abler <alinea@riseup.net>
2017-11-24 00:11:07 +0100

    More modularization and first two poll options

---
+++ basic_poll_handler.py
@@ -0,0 +1,41 @@
+max_options = 10
+
+
+def options(poll):
+    buttons = []
+    for i, option in enumerate(poll['options']):
+        votes = num_votes(poll, i)
+        buttons.append([{
+            'text': "{}{}{}".format(option['text'],
+                                    " - " if votes > 0 else "",
+                                    votes if votes > 0 else ""),
+             'callback_data': {'i': i},
+        }])
+    return buttons
+
+
+def title(poll):
+    return "*{}*".format(poll['title'])
+    
+
+def evaluation(poll):
+    message = ""
+    for i, option in enumerate(poll['options']):
+        message += "\n"
+        message += "{}: {}".format(option['text'], num_votes(poll, i))
+    return message
+
+
+def handle_vote(votes, user, callback_data):
+    old_vote = None
+    if user in votes:
+        old_vote = votes.pop(user)
+    if old_vote is not None and str(old_vote) == str(callback_data['i']):
+        # remove old vote
+        pass
+    else:
+        votes[user] = callback_data['i']
+
+
+def num_votes(poll, i):
+    return list(poll['votes'].values()).count(i) if 'votes' in poll else 0
